4e41f942083272d9b12c578b5a861498106ff756
KyleColorado <r.kyle.ussery@gmail.com>
2024-05-09 21:04:44 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// Basic JavaScript to interact with the Web Audio API
+function playArpeggio() {
+    // Function to generate and play arpeggio
+    console.log('Arpeggio played'); // Replace with actual code
+}
